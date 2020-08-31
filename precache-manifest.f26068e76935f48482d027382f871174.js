10ea0166bbdf45994c6be20979db769b59f48f81
priyasarpate <sarpatepriya@gmail.com>
2020-08-31 15:26:49 +0530

    Updates

---
+++ precache-manifest.f26068e76935f48482d027382f871174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91029dd06b83e50140e64fff618e8b26",
+    "url": "/covi19/index.html"
+  },
+  {
+    "revision": "c55d3a75ce400d04b86d",
+    "url": "/covi19/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "80d36dd9226062b9fbaa",
+    "url": "/covi19/static/css/main.dd0bde0b.chunk.css"
+  },
+  {
+    "revision": "c55d3a75ce400d04b86d",
+    "url": "/covi19/static/js/2.f7b2432a.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covi19/static/js/2.f7b2432a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80d36dd9226062b9fbaa",
+    "url": "/covi19/static/js/main.52be3eb6.chunk.js"
+  },
+  {
+    "revision": "c0fa92fcec8fadce8d49",
+    "url": "/covi19/static/js/runtime-main.3a66874a.js"
+  }
+]);
