cd37d102ba74e7c63ec8d30541228c62b1169135
Gunpriya Sarpate <sarpatepriya@gmail.com>
2020-11-09 23:29:03 +0530

    style-cards

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import { Card, CardContent, Typography } from '@material-ui/core';
-import './InfoBox.css';
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
-function InfoBox({title, cases, total,active, isRed, ...props}){
-    return(
-        <div>
-             <Card 
-             onClick={props.onClick}
-             className={`infoBox ${active && "infoBox--selected"} ${
-                isRed && "infoBox--red"
-              }`}
-            >
-             
-                 <CardContent>
-                     {/* title */}
-                    <Typography className='infoBox__title' color='textSecondary'>{title}</Typography>
-                     {/* number of cases */}
-                     <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>{cases}</h2>
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
 
-                     {/* total */}
-                     <Typography className='infoBox__total' color='textSecondary'>{total} Total</Typography>
-                 </CardContent>
-
-             </Card>
-        </div>
-    )
-
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default InfoBox;
